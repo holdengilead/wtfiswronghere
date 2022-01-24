b096d84e81b87a7875a0f7215fd058fa05ee8213
Alberto B <criptoholden@gmail.com>
2022-01-24 18:02:56 +0100

    Challenge 05.

--- 05_challenge/05_challenge.py
+++ 05_challenge/05_challenge.py
@@ -10,29 +10,31 @@ For all integers between 1 and 99 (include both):
     # print fizzbuzz for multiples of 3 and 5"
 """
 
+
 def fizzbuzz(max_num):
     "This method implements FizzBuzz"
-    
+
     # adding some redundant declarations on purpose
     # we will make our script 'tighter' in one of coming exercises
-    three_mul = 'fizz'
-    five_mul = 'buzz'
-    with open('mifile.txt','r') as f:
-        print 'i have created'
-        num1 = int(f.readline())   
-        num2=int(f.readline())        
+    three_mul = "fizz"
+    five_mul = "buzz"
+    with open("myfile.txt", "r") as f:
+        print("i have created")
+        num1 = int(f.readline())
+        num2 = int(f.readline())
         max_num = int(f.readline())
-         
-    # Google for 'range in python' to see what it does
-    for i in range(1,max_num):
-        # % or modulo division gives you the remainder 
-        if i%num1==0 and i%num2==0:
-            print(i,three_mul+five_mul)
-        elif i%num1==0:
-            print(i,three_mul)
-        elif i%num2==0:
-            print(i,five_mul)
 
-#----START OF SCRIPT
-if __name__=='__main__':
+    # Google for 'range in python' to see what it does
+    for i in range(1, max_num):
+        # % or modulo division gives you the remainder
+        if i % num1 == 0 and i % num2 == 0:
+            print(i, three_mul + five_mul)
+        elif i % num1 == 0:
+            print(i, three_mul)
+        elif i % num2 == 0:
+            print(i, five_mul)
+
+
+# ----START OF SCRIPT
+if __name__ == "__main__":
     fizzbuzz(100)
---
+++ 05_challenge/05_readme.md
@@ -0,0 +1,2 @@
+'print' is a function.
+The name of the file is 'myfile.txt'
